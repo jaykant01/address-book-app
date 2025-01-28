8265fda98a811887705b3d8b56ecb815711037df
jaykant02 <jaykanty02@gmail.com>
2025-01-28 23:09:50 +0530

    completed and defined routes of register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HeroComponent} from '../hero/hero.component';
+import {RegisterComponent} from '../register/register.component';
 
 export const routes: Routes = [
-  {path: '', component: HeroComponent, title: 'AddressBookApp'}
+  {path: '', component: HeroComponent, title: 'AddressBookApp'},
+  {path: 'register', component: RegisterComponent, title: 'Register'}
 ];
