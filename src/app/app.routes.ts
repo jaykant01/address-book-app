1c9e0e22b58c220c9b70bb58377be0f122a2c420
jaykant02 <jaykanty02@gmail.com>
2025-01-28 23:15:42 +0530

    defined routes & completed the code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HeroComponent} from '../hero/hero.component';
 import {RegisterComponent} from '../register/register.component';
+import {LoginComponent} from '../login/login.component';
 
 export const routes: Routes = [
   {path: '', component: HeroComponent, title: 'AddressBookApp'},
-  {path: 'register', component: RegisterComponent, title: 'Register'}
+  {path: 'register', component: RegisterComponent, title: 'Register'},
+  {path: 'login', component: LoginComponent, title: 'Login'}
 ];
